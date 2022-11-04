ebf74c9f7376048d9dac93fb7002d9231749a381
SANGWOO PARK <cyberprophet@hanmail.net>
2022-11-05 03:13:42 +0900

    초기 커밋.

---
+++ Algorithmic.Server/Server/Pages/Error.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.Diagnostics;
+
+namespace ShareInvest.Server.Pages
+{
+    [IgnoreAntiforgeryToken,
+     ResponseCache(Duration = 0,
+                   Location = ResponseCacheLocation.None,
+                   NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        public string? RequestId
+        {
+            get; set;
+        }
+        public bool ShowRequestId
+        {
+            get
+            {
+                logger.LogInformation(nameof(ShowRequestId), RequestId);
+
+                return string.IsNullOrEmpty(RequestId) is false;
+            }
+        }
+        public ErrorModel(ILogger<ErrorModel> logger) => this.logger = logger;
+
+        public void OnGet() => RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+        readonly ILogger<ErrorModel> logger;
+    }
+}
