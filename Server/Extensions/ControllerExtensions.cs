b40448e77f4274fa495b9fe3747983ca15b296f3
SANGWOO PARK <cyberprophet@hanmail.net>
2022-11-13 01:01:44 +0900

    struggle to connect the server to the client

--- Server/Extensions/ControllerExtensions.cs
+++ Server/Extensions/ControllerExtensions.cs
@@ -11,73 +11,72 @@ public static class ControllerExtensions
     {
         var paramTransformer = new SlugifyParameterTransformer();
 
-        builder.Services
-               .AddSwaggerGen(o =>
-               {
-                   o.SwaggerDoc("stock", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("user", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("account", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("balance", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("message", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-               })
-               .AddControllersWithViews(o =>
-               {
-                   o.Conventions.Add(new RouteTokenTransformerConvention(paramTransformer));
-               })
-               .AddJsonOptions(o =>
-               {
-                   o.JsonSerializerOptions.WriteIndented = Status.IsDebugging;
-               });
+        builder.Services.AddSwaggerGen(o =>
+        {
+            o.SwaggerDoc("stock", new OpenApiInfo
+            {
+                Contact = new OpenApiContact
+                {
+                    Name = "Algorithmic-Trading",
+                    Url = new Uri("http://shareinvest.net")
+                },
+                Description = "this version that integrates all environments",
+                TermsOfService = new Uri("http://shareinvest.net/terms"),
+                Title = "Algorithmic-Trading-Server"
+            });
+            o.SwaggerDoc("user", new OpenApiInfo
+            {
+                Contact = new OpenApiContact
+                {
+                    Name = "Algorithmic-Trading",
+                    Url = new Uri("http://shareinvest.net")
+                },
+                Description = "this version that integrates all environments",
+                TermsOfService = new Uri("http://shareinvest.net/terms"),
+                Title = "Algorithmic-Trading-Server"
+            });
+            o.SwaggerDoc("account", new OpenApiInfo
+            {
+                Contact = new OpenApiContact
+                {
+                    Name = "Algorithmic-Trading",
+                    Url = new Uri("http://shareinvest.net")
+                },
+                Description = "this version that integrates all environments",
+                TermsOfService = new Uri("http://shareinvest.net/terms"),
+                Title = "Algorithmic-Trading-Server"
+            });
+            o.SwaggerDoc("balance", new OpenApiInfo
+            {
+                Contact = new OpenApiContact
+                {
+                    Name = "Algorithmic-Trading",
+                    Url = new Uri("http://shareinvest.net")
+                },
+                Description = "this version that integrates all environments",
+                TermsOfService = new Uri("http://shareinvest.net/terms"),
+                Title = "Algorithmic-Trading-Server"
+            });
+            o.SwaggerDoc("message", new OpenApiInfo
+            {
+                Contact = new OpenApiContact
+                {
+                    Name = "Algorithmic-Trading",
+                    Url = new Uri("http://shareinvest.net")
+                },
+                Description = "this version that integrates all environments",
+                TermsOfService = new Uri("http://shareinvest.net/terms"),
+                Title = "Algorithmic-Trading-Server"
+            });
+        })
+            .AddControllersWithViews(o =>
+            {
+                o.Conventions.Add(new RouteTokenTransformerConvention(paramTransformer));
+            })
+            .AddJsonOptions(o =>
+            {
+                o.JsonSerializerOptions.WriteIndented = Status.IsDebugging;
+            });
         return builder;
     }
 }
---
+++ Server/Extensions/SignalRExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http.Connections;
+
+using ShareInvest.Server.Hubs;
+
+namespace ShareInvest.Server.Extensions;
+
+public static class SignalRExtensions
+{
+    public static WebApplicationBuilder ConfigureHubs(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSignalR(o =>
+        {
+            o.EnableDetailedErrors = true;
+        });
+        return builder;
+    }
+    public static WebApplication ConfigureHubs(this WebApplication app)
+    {
+        app.MapHub<Kiwoom>(app.Configuration["Hubs:Kiwoom"], o =>
+        {
+            o.Transports = HttpTransportType.WebSockets |
+                           HttpTransportType.LongPolling;
+        });
+        return app;
+    }
+}
