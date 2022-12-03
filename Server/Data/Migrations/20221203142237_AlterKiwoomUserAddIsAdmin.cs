d4ee83b3b79fd86a005cdab84d0d195cdce24232
SANGWOO PARK <cyberprophet@hanmail.net>
2022-12-03 23:36:52 +0900

    designate the manager

---
+++ Server/Data/Migrations/20221203142237_AlterKiwoomUserAddIsAdmin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShareInvest.Server.Data.Migrations
+{
+    public partial class AlterKiwoomUserAddIsAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdministrator",
+                table: "KiwoomUser",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdministrator",
+                table: "KiwoomUser");
+        }
+    }
+}
