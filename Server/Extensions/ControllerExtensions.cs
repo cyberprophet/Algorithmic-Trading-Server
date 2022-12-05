b0e95b9540defd26018d57269fee6cdf8f81f765
SANGWOO PARK <cyberprophet@hanmail.net>
2022-12-06 02:00:37 +0900

    remove unnecessary code

--- Server/Extensions/ControllerExtensions.cs
+++ Server/Extensions/ControllerExtensions.cs
@@ -9,77 +9,28 @@ public static class ControllerExtensions
 {
     public static WebApplicationBuilder ConfigureControllers(this WebApplicationBuilder builder)
     {
-        var paramTransformer = new SlugifyParameterTransformer();
+        SlugifyParameterTransformer paramTransformer = new();
 
+        OpenApiInfo openApiInfo = new()
+        {
+            Contact = new OpenApiContact
+            {
+                Name = "Algorithmic-Trading",
+                Url = new Uri("http://shareinvest.net")
+            },
+            Description = "this version that integrates all environments",
+            TermsOfService = new Uri("http://shareinvest.net/terms"),
+            Title = "Algorithmic-Trading-Server"
+        };
         builder.Services
                .AddSwaggerGen(o =>
                {
-                   o.SwaggerDoc("stock", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("user", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("account", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("balance", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("message", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
-                   o.SwaggerDoc("file", new OpenApiInfo
-                   {
-                       Contact = new OpenApiContact
-                       {
-                           Name = "Algorithmic-Trading",
-                           Url = new Uri("http://shareinvest.net")
-                       },
-                       Description = "this version that integrates all environments",
-                       TermsOfService = new Uri("http://shareinvest.net/terms"),
-                       Title = "Algorithmic-Trading-Server"
-                   });
+                   o.SwaggerDoc("stock", openApiInfo);
+                   o.SwaggerDoc("user", openApiInfo);
+                   o.SwaggerDoc("account", openApiInfo);
+                   o.SwaggerDoc("balance", openApiInfo);
+                   o.SwaggerDoc("message", openApiInfo);
+                   o.SwaggerDoc("file", openApiInfo);
                })
                .AddControllersWithViews(o =>
                {
